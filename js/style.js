751c0b687526460f09c8365fb49ca53259ba1fe2
DableBlock <1255887634@qq.com>
2017-06-15 01:23:34 +0800

    111
    111

---
+++ js/style.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#top #top_main .top_right li .img1').hover(function(){
+		$(this).css('background-position','0px -15px');
+	},
+	function(){
+		$(this).css('background-position','0px 0px');
+	});
+	
+});
